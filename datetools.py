804198e2a70c483d9ff358e242d93cc8758f276f
Matt Bay <MJBayUNL@yahoo.com>
2023-01-03 20:36:05 -0600

    added leapYear check to datetools.py

--- datetools.py
+++ datetools.py
@@ -2,3 +2,14 @@ class DateTools:
 
     def __init__(self):
         pass
+
+    def leapYear(year:int) -> bool:
+        if year%4 != 0:
+            return False
+
+        if year%100 == 0 and year%400 != 0:
+            return False
+            
+        return True
+
+    
