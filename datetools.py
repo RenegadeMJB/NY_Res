c2e8c69bc69de466e0b28390e80fe699508b02d6
Matt Bay <MJBayUNL@yahoo.com>
2023-01-03 22:19:45 -0600

    started printMonth

--- datetools.py
+++ datetools.py
@@ -1,8 +1,19 @@
 class DateTools:
     firstYear = 2021
     days = ('Fri', 'Sat', 'Sun', 'Mon', 'Tues', 'Wed', 'Thur')
+    daysInMonth = (31, 28, 31, 30, 31, 30, 31, 31, 31, 31, 30, 31)
+    monthString = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+
     def __init__(self):
         pass
+
+    def clearscrn():
+        print('\33[0;0H')
+        for i in range(0,25):
+            for j in range (0,81):
+                print(f'\33[{i};{j}H ', flush=True, end='')
+
+        print('\33[0;0H', flush=True, end='')
 
     def leapYear(year:int) -> bool:
         if year%4 != 0:
@@ -25,3 +36,9 @@ class DateTools:
         day = totalDays%7
 
         return day
+
+    def printMonth(month:int, year:int):
+        DateTools.clearscrn()
+        
+        print(f'  {DateTools.monthString[month]}   ')
+        print('S M T W T F S')
