191a799dd1dae7a5835773b48474acf0bcb7046e
Matt Bay <MJBayUNL@yahoo.com>
2023-01-04 21:35:57 -0600

    began the day class that will run the program

--- NY_Res.py
+++ NY_Res.py
@@ -1,6 +1,13 @@
 from datetools import DateTools as DT
 import os
 
+class Day:
+    def __init__(self, numberResolutions):
+        self._finished = []
+        for i in range(0, numberResolutions):
+            self._finished.append(False)
+
+
 def main():
     cmd = """osascript -e '
     tell application "Terminal"
