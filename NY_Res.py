e01bbbde9741bd83dc6be6e8e5627cf6d408f793
Matt Bay <MJBayUNL@yahoo.com>
2023-01-05 18:18:42 -0600

    copied over the printYear and printMonth methods to the year class

--- NY_Res.py
+++ NY_Res.py
@@ -33,13 +33,84 @@ class Day:
 
         if done:
             self._color = Day.green
-            print(f'{self._color}', flush=True, end='')
+            #print(f'{self._color}', flush=True, end='')
         elif started:
             self._color = Day.yellow
-            print(f'{self._color}',flush=True,end='')
+            #print(f'{self._color}',flush=True,end='')
 
 
-    
+class Year:
+    def __init__(self, year, numberResolutions, days = []):
+        self._year = year
+        self._days = []
+        self._numberResolutions = numberResolutions
+        for day in days:
+            self._day.append(day)
+        
+    def addDay(self):
+        day = Day(self._numberResolutions)
+        self._days.append(day)
+
+    def printYear(self):
+        DT.clearscrn()
+        row = 1
+        column = 1
+        days = 0
+        for month in range(0,12):
+            '''This is messy! I have the printMonth method also returning
+            the days in the month so that I can keep track of how many days it has been.
+            This isn't going to work though! This little thing is getting wildly out
+            wing!'''
+            days += DT.printMonth(month, self._year, row, column)
+            column += 24
+            if (month + 1) % 4 == 0:
+                if month != 0:
+                    column = 1
+                    row += 9
+
+    def printMonth(self, month:int, rowStart = 1, columnStart = 1):
+        #DT.clearscrn()
+        
+        Col = columnStart
+        Row = rowStart
+
+        print(f'\33[{Row};{Col}H         {DT.monthString[month]}      ')
+        Row += 1
+        print(f'\33[{Row};{Col}H S  M  T  W  T  F  S ')
+        Row += 1
+
+        day = DT.fdoyear(self._year)
+        leapYear = DT.leapYear(self._year)
+
+        #this is figuring out how many days up to that month not including the month
+        days = 0
+        for i in range(0, month):
+            days += DT.daysInMonth[i]
+            if i == 1 and leapYear:
+                days += 1
+
+        firstDay = (days%7 + day) % 7
+        
+        for i in range(0, 3*firstDay):
+            print(f'\33[{Row};{Col}H ')
+            Col += 1
+
+        for dayNumber in range(1, DT.daysInMonth[month] + 1):
+            print(f'\33[{Row};{Col}H{dayNumber:2} ')
+            Col += 3
+            colRelative = Col - columnStart
+            if colRelative % 21 == 0:
+                if colRelative != 3:
+                    Row += 1
+                    Col = columnStart
+
+        if month == 1 and leapYear:
+            leapDay = 29
+            print(f'\33[{Row};{Col}H{leapDay:2} ')
+            return (DT.daysInMonth[month] + 1)
+
+
+
             
 
 def main():
